212fb619516f8f3c738d1a209c36ab1c688a2a87
Anjali Tanna <85650610+anjali-tanna@users.noreply.github.com>
2023-09-12 16:54:31 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ds4200-recipes
-DS4300 Large-Scale Data Storage and Retrieval Recipe Recommendation Engine
+# Recipe Recommendation System
+
+### DS4300 Final Project Spring 2023
+Ava Duggan, Max Rizzuto, Julian Savini, Anjali Tanna
+
+Click below to watch us use the engine!
+
+
+[DEMO VIDEO](https://drive.google.com/file/d/1TiQC1zzqniXXINrGcj9z7QzKvLYAtVs-/view?usp=sharing)
+
+
+### Purpose: 
+- The main purpose of this project is to create an interactive recipe recommendation system that allows users to input information when prompted on a website, in order to output a recipe that will align with a user’s constraints. The goal is to use information that can be found on recipe websites such as recipe type, cooking technique, cuisine, etc. which can indicate what type of meal a user is looking to cook. The target users for this project are people who are open to having recipe recommendations based on factors that can limit what a user is able to cook such as available ingredients, time constraints, etc. 
+- Additionally, this can be a tool for planning ahead, where a user can know what ingredients they may want to buy from the grocery store in order to make a desired recipe. A recommendation system such as this can help to prevent food waste as well as assist people in finding quick, easy and healthy recipes when cooking can often be seen as time consuming and challenging. 
+
+### Tech Stack
+- Python 
+  - Front end developement
+  - Web scraping
+  - Data cleaning
+  - Connections with MongoDB and Neo4j
+  - Querying
+
+- MongoDB
+  - Storing `recipe_data.json`
+
+- Neo4j
+  - Creating [:SIMILAR] relationships between recipes
+  - Creating recommendations
---
+++ scraping.py
@@ -0,0 +1,97 @@
+import requests
+import json
+
+
+def scrape_recipe(dct):
+    seo = dct['seo']
+    dct = dct['feed'][0]
+
+    recipe = dict()
+    # get recipe metadata
+    recipe['name'] = dct['display']['displayName']
+    recipe['id'] = dct['content']['details']['recipeId']
+    recipe['url'] = seo['web']['link-tags'][0]['href']
+    recipe['recipeType'] = dct['recipeType'][0]
+
+    recipe['keywords'] = dct['seo']['spotlightSearch']['keywords']
+
+    # recipe description
+    if dct['content']['description']:
+        recipe['description'] = dct['content']['description']['text']
+    else:
+        recipe['description'] = 'N/A'
+
+    # recipe steps
+    if dct['content']['preparationSteps']:
+        recipe['steps'] = [x for x in dct['content']['preparationSteps']]
+    else:
+        recipe['steps'] = []
+
+    # get recipe content
+    tags = dct['content']['tags'].keys()
+    all_tags = ['dish', 'course', 'technique', 'cuisine']
+    for tag in all_tags:
+        if tag in tags:
+            recipe[tag] = [x['display-name'] for x in dct['content']['tags'][tag]]
+        else:
+            recipe[tag] = list()
+
+    # get ingredient data
+    recipe['ingredients'] = []
+    for ingredient in dct['content']['ingredientLines']:
+        ing_dct = dict()
+        ing_dct['name'] = ingredient['ingredient']
+        ing_dct['category'] = ingredient['category']
+
+        if not ingredient['quantity']:
+            ing_dct['quantity'] = {'metric': 'N/A', 'imperial': 'N/A'}
+        else:
+            ing_dct['quantity'] = {
+                'metric': str(ingredient['amount']['metric']['quantity']) + ' ' +
+                          ingredient['amount']['metric']['unit']['abbreviation'],
+                'imperial': str(ingredient['amount']['imperial']['quantity']) + ' ' +
+                            ingredient['amount']['imperial']['unit']['abbreviation'],
+            }
+
+        recipe['ingredients'].append(ing_dct)
+
+    # get review data
+    recipe['avgRating'] = dct['content']['reviews']['averageRating']
+    recipe['numReviews'] = dct['content']['reviews']['totalReviewCount']
+
+    # get nutritional data
+    recipe['nutrition'] = list()
+    for nut in dct['content']['nutrition']['nutritionEstimates']:
+        nut_dct = dict()
+        nut_dct['name'] = nut['attribute']
+        nut_dct['value'] = f"{nut['display']['value']} {nut['unit']['abbreviation']}"
+        nut_dct['pctDailyValue'] = nut['display']['percentDailyValue']
+        recipe['nutrition'].append(nut_dct)
+
+    return recipe
+
+
+def scrape_feed(total=499, limit=50):
+    # set urls to scrape through
+    start_urls = [f'https://mapi.yummly.com/mapi/v19/content/feed?start={x}&limit=50&allowedContent=single_recipe' for x
+                  in range(0, total, limit)]
+
+    # get each page in the feed, get ids, and call scrape recipe for each recipe
+    recipe_data = list()
+    for url in start_urls:
+        dct = requests.get(url).json()
+        ids = [x['tracking-id'].split(',')[0].strip('recipe:') for x in dct['feed']]
+        for id in ids:
+            dct = requests.get(f'https://mapi.yummly.com/mapi/v19/content/feed?id={id}').json()
+            if dct and dct['feed']:
+                recipe_data.append(scrape_recipe(dct))
+    return recipe_data
+
+
+def main():
+    dct = list({x['id']: x for x in scrape_feed()}.values())
+    with open("data/recipe_data.json", "w") as final:
+        json.dump(dct, final)
+
+
+main()
